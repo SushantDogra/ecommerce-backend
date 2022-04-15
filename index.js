0e8209c7cf21ee6a621648b74617d2714e438d88
SushantDogra <sushantsingh.dogra@gmail.com>
2022-04-15 16:20:20 +0530

    initialize nodejs app

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+const express = require("express");
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/status", (req, res) => {
+  res.send("OK");
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
+});
